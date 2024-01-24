ff4b06d18ace8ba52afaf0340de5da92917799c0
VincentFerrigan <vincent.ferrigan@gmail.com>
2024-01-24 10:09:04 +0100

    Generalized headers

--- tasks/2/mymaps/data/ratio.py
+++ tasks/2/mymaps/data/ratio.py
@@ -16,13 +16,16 @@ def read_data(filename, operation):
 def calculate_ratios(data1, data2):
     return {size: (data1[size] / data2[size] if size in data2 and data2[size] != 0 else 0)
             for size in data1}
+# def calculate_ratios(data1, data2):
+#     return {size: round(data1[size] / data2[size], 2) if size in data2 and data2[size] != 0 else 0
+#             for size in data1}
 
-def write_combined_data_to_file(list_data, tree_data, map_data, ratios_tl, ratios_lm, ratios_tm, operation, output_file):
-    cap_op = operation[0].upper() + operation[1:]
+
+def write_combined_data_to_file(list_data, tree_data, map_data, ratios_tl, ratios_lm, ratios_tm, output_file):
     with open(output_file, 'w') as file:
         writer = csv.writer(file, delimiter=' ')
-        writer.writerow(['Size', 'List {0}'.format(cap_op), 'Tree {0}'.format(cap_op), 'Map {0}'.format(cap_op),
-                         'Tree/List Ratio', 'List/Map Ratio', 'Tree/Map Ratio'])
+        writer.writerow(['Size', 'List', 'Tree', 'Map',
+                         'List:Tree', 'List:Map', 'Tree:Map'])
 
         sizes = set(list_data.keys()) | set(tree_data.keys()) | set(map_data.keys())
         for size in sorted(sizes):
@@ -42,13 +45,13 @@ def write_combined_ratios_to_files_per_operation(operation):
     map_data = read_data('map.dat', operation)
 
     # Calculate ratios
-    ratios_tree_list = calculate_ratios(tree_data, list_data)
+    ratios_tree_list = calculate_ratios(list_data, tree_data)
     ratios_list_map = calculate_ratios(list_data, map_data)
     ratios_tree_map = calculate_ratios(tree_data, map_data)
 
     # Write combined data to a new file
     write_combined_data_to_file(list_data, tree_data, map_data, ratios_tree_list, ratios_list_map, ratios_tree_map,
-                                operation, 'combined_{0}_ratios.dat'.format(operation))
+                                'combined_{0}_ratios.dat'.format(operation))
 
 write_combined_ratios_to_files_per_operation("add")
 write_combined_ratios_to_files_per_operation("lookup")
