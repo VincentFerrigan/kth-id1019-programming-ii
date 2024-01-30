fa885a654c09cb5f580b9f13038b96ddbef4343e
VincentFerrigan <vincent.ferrigan@gmail.com>
2024-01-30 11:13:41 +0100

    Added comments, spec and docs

--- tasks/3/expression/lib/expression.ex
+++ tasks/3/expression/lib/expression.ex
@@ -1,7 +1,14 @@
 defmodule Expression do
   @moduledoc """
   Documentation for `Expression`.
+
+  This module provides functionality for evaluating mathematical expressions,
+  including operations like addition, subtraction, multiplication, and division.
+  It supports handling literals (variables, whole numbers and rational numbers),
+  ensuring correct arithmetic operations and simplification of expressions.
   """
+
+
 
   @typedoc """
   A `literal` in the expression. It can be:
@@ -25,6 +32,14 @@ defmodule Expression do
                   | {:div, expr(), expr()}
                   | literal()
 
+  @doc """
+  Evaluates and prints given expression based on provided environment.
+
+  This function takes a mathematical expression represented in a structured format (as defined by the `expr()` type),
+  and an environment,`env`, as a mapping of variables to their values.
+
+  The given expression and the final simplified evaluation are pretty printed to the console.
+  """
   @spec run(expr(), map()) :: :ok
   def run(ast, env) do
     IO.write("Evaluate #{pretty_print(ast)}, \n where #{inspect(env)}.\n\n")
@@ -34,7 +49,12 @@ defmodule Expression do
     :ok
   end
 
-  @spec eval(expr(), map()):: expr()
+  @doc """
+  Evaluates the given expression based on the provided environment.
+  It takes an expression as an abstract syntax tress, `ast`,
+  and an environment,`env`, as a mapping of variables to their values.
+  """
+  @spec eval(expr(), map()):: literal()
   def eval({:num, n}, _), do: {:num, n}
   def eval({:var, v}, env), do: {:num, Map.get(env, v)}
   def eval({:add, e1, e2}, env), do: addition(eval(e1, env), eval(e2, env))
@@ -46,21 +66,27 @@ defmodule Expression do
     division(eval({:num, a}, env), eval({:num, b}, env))
   end
 
+  # Performs addition between two expressions, handling both numeric and rational types.
   defp addition({:q, a, b}, {:q, c, d}), do: division({:num, a * d + b * c}, {:num, b * d})
   defp addition({:num, a}, {:q, c, d}), do: division({:num, a * d + 1 * c}, {:num, 1 * d})
   defp addition({:q, a, b}, {:num, c}), do: division({:num, a * 1 + b * c}, {:num, b * 1})
   defp addition({:num, a}, {:num, c}), do: {:num, a + c}
 
+  # Performs subtraction between two expressions, handling both numeric and rational types.
   defp subtraction({:q, a, b}, {:q, c, d}), do: division({:num, a * d - b * c}, {:num, b * d})
   defp subtraction({:num, a}, {:q, c, d}), do: division({:num, a * d - 1 * c}, {:num, 1 * d})
   defp subtraction({:q, a, b}, {:num, c}), do: division({:num, a * 1 - b * c}, {:num, b * 1})
   defp subtraction({:num, a}, {:num, c}), do: {:num, a - c}
 
+  # Performs multiplication between two expressions, handling both numeric and rational types.
   defp multiplication({:q, a, b}, {:q, c, d}), do: division({:num, a * c}, {:num, b * d})
   defp multiplication({:num, a}, {:q, c, d}), do: division({:num, a * c}, {:num, 1 * d})
   defp multiplication({:q, a, b}, {:num, c}), do: division({:num, a * c}, {:num, b * 1})
   defp multiplication({:num, a}, {:num, c}), do: {:num, a * c}
 
+
+  # Performs division between two expressions, handling both numeric and rational types.
+  # Raises an ArgumentError if an attempt is made to divide by zero.
   defp division({:q, a, b}, {:q, c, d}) do
     multiplication({:q, a, b}, {:q, d, c})
   end
@@ -69,10 +95,12 @@ defmodule Expression do
   end
 
   defp division({:num, n1}, {:num, n2}) do
+    # Simplifies the result by dividing both numerator and denominator by their GCD.
     gcd = Integer.gcd(convert_float_to_int_if_whole(n1), convert_float_to_int_if_whole(n2))
     {:q, convert_float_to_int_if_whole(n1/gcd), convert_float_to_int_if_whole(n2/gcd)}
   end
 
+  # Converts a float to an integer if it's a whole number, otherwise raises an ArgumentError.
   defp convert_float_to_int_if_whole(float) do
     truncated = trunc(float)
     if truncated == float do
@@ -84,18 +112,18 @@ defmodule Expression do
 
   # Pretty printing
   @spec pretty_print(expr()) :: String.t()
-  def pretty_print({:num, n}), do: "#{n}"
-  def pretty_print({:var, v}), do: "#{v}"
-  def pretty_print({:mul, {:num, n}, {:var, v}}), do: "#{n}#{v}"
-  def pretty_print({:mul, {:var, v}, {:num, n}}), do: "#{n}#{v}"
+  defp pretty_print({:num, n}), do: "#{n}"
+  defp pretty_print({:var, v}), do: "#{v}"
+  defp pretty_print({:mul, {:num, n}, {:var, v}}), do: "#{n}#{v}"
+  defp pretty_print({:mul, {:var, v}, {:num, n}}), do: "#{n}#{v}"
 
-  def pretty_print({:mul, {:num, -1}, e}), do: "-(#{pretty_print(e)})"
-  def pretty_print({:mul, e, {:num, -1}}), do: "-(#{pretty_print(e)})"
-  def pretty_print({:add, e1, e2}), do: "#{pretty_print(e1)} + #{pretty_print(e2)}"
-  def pretty_print({:div, e1, e2}), do: "#{pretty_print(e1)}/#{pretty_print(e2)}"
-  def pretty_print({:q, e1, e2}), do: "#{pretty_print(e1)}/#{pretty_print(e2)}"
-  def pretty_print({:mul, e1, e2}), do: "#{pretty_print(e1)} * #{pretty_print(e2)}"
+  defp pretty_print({:mul, {:num, -1}, e}), do: "-(#{pretty_print(e)})"
+  defp pretty_print({:mul, e, {:num, -1}}), do: "-(#{pretty_print(e)})"
+  defp pretty_print({:add, e1, e2}), do: "#{pretty_print(e1)} + #{pretty_print(e2)}"
+  defp pretty_print({:div, e1, e2}), do: "#{pretty_print(e1)}/#{pretty_print(e2)}"
+  defp pretty_print({:q, e1, e2}), do: "#{pretty_print(e1)}/#{pretty_print(e2)}"
+  defp pretty_print({:mul, e1, e2}), do: "#{pretty_print(e1)} * #{pretty_print(e2)}"
 
   @spec pretty_print(number()) :: String.t()
-  def pretty_print(n), do: "#{n}"
+  defp pretty_print(n), do: "#{n}"
 end
