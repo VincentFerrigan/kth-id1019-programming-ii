d3396d43b62c3ca54837269a9305183dcc03ac0a
VincentFerrigan <vincent.ferrigan@gmail.com>
2024-01-31 19:53:23 +0100

    Adding tests

--- tasks/3/expression/test/expression_test.exs
+++ tasks/3/expression/test/expression_test.exs
@@ -16,11 +16,26 @@ defmodule ExpressionTest do
     assert Expression.eval(e, env) == {:num, 5}
   end
 
-  test "Evaluate 3x/2x, where x = 2" do
-    e = {:div, {:mul, {:num, 3}, {:var, :x}}, {:mul, {:num, 2}, {:var, :x}}}
-    env = Map.new()
-    env = Map.put(env, :x, 2)
+  test "Evaluate x, where x = -5" do
+    assert Expression.eval({:var, :x}, %{x: -5}) == {:num, -5}
+  end
+
+  test "Evaluate 3x/4, where x = 2" do
+    e = {:div, {:mul, {:num, 3}, {:var, :x}}, {:num, 4}}
+    env = %{x: 2}
     assert Expression.eval(e, env) == {:q, 3, 2}
+  end
+
+  test "Evaluate 3x/2y, where x = 2, y= 4" do
+    e = {:div, {:mul, {:num, 3}, {:var, :x}}, {:mul, {:num, 2}, {:var, :y}}}
+    env = %{x: 2, y: 4}
+    assert Expression.eval(e, env) == {:q, 3, 4}
+  end
+
+  test "Evaluate 3x/2y, where x = 2, y= 3" do
+    e = {:div, {:mul, {:num, 3}, {:var, :x}}, {:mul, {:num, 2}, {:var, :y}}}
+    env = %{x: 2, y: 3}
+    assert Expression.eval(e, env) == {:num, 1}
   end
 
   test "Evaluate 2x + 3 + 1/2, where x = 2" do
