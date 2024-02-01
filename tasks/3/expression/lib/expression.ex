f6e25bbd2fc72526232b14eefd052e1b015f164f
VincentFerrigan <vincent.ferrigan@gmail.com>
2024-02-01 15:42:19 +0100

    Rewrote comments

--- tasks/3/expression/lib/expression.ex
+++ tasks/3/expression/lib/expression.ex
@@ -8,8 +8,6 @@ defmodule Expression do
   ensuring correct arithmetic operations and simplification of expressions.
   """
 
-
-
   @typedoc """
   A `literal` in the expression. It can be:
     - `{:num, number()}`: A numeric literal.
@@ -52,13 +50,12 @@ defmodule Expression do
   def run(ast, env) do
     IO.write("Evaluating #{pretty_print(ast)}, where #{inspect(env)}.\n")
     IO.write("Result: #{pretty_print(eval(ast, env))}\n")
-
-    # TODO replace inspect. Not directly pretty!!
     :ok
   end
+  # TODO replace inspect. Not directly pretty!!
 
   @doc """
-  Evaluates the given expression based on the provided environment.
+  Evaluate given expression based on the provided environment.
   It takes an expression as an abstract syntax tress, `ast`,
   and an environment,`env`, as a mapping of variables to their values.
 
@@ -83,27 +80,26 @@ defmodule Expression do
     division(eval({:num, a}, env), eval({:num, b}, env))
   end
 
-  # Performs addition between two expressions, handling both numeric and rational types.
+  # Perform addition between two expressions, handling both integers and rational numbers
   defp addition({:q, a, b}, {:q, c, d}), do: division({:num, a * d + b * c}, {:num, b * d})
   defp addition({:num, a}, {:q, c, d}), do: division({:num, a * d + 1 * c}, {:num, 1 * d})
   defp addition({:q, a, b}, {:num, c}), do: division({:num, a * 1 + b * c}, {:num, b * 1})
   defp addition({:num, a}, {:num, c}), do: {:num, a + c}
 
-  # Performs subtraction between two expressions, handling both numeric and rational types.
+  # Perform subtraction between two expressions, handling both integers and rational numbers
   defp subtraction({:q, a, b}, {:q, c, d}), do: division({:num, a * d - b * c}, {:num, b * d})
   defp subtraction({:num, a}, {:q, c, d}), do: division({:num, a * d - 1 * c}, {:num, 1 * d})
   defp subtraction({:q, a, b}, {:num, c}), do: division({:num, a * 1 - b * c}, {:num, b * 1})
   defp subtraction({:num, a}, {:num, c}), do: {:num, a - c}
 
-  # Performs multiplication between two expressions, handling both numeric and rational types.
+  # Perform multiplication between two expressions, handling both integers and rational numbers.
   defp multiplication({:q, a, b}, {:q, c, d}), do: division({:num, a * c}, {:num, b * d})
   defp multiplication({:num, a}, {:q, c, d}), do: division({:num, a * c}, {:num, 1 * d})
   defp multiplication({:q, a, b}, {:num, c}), do: division({:num, a * c}, {:num, b * 1})
   defp multiplication({:num, a}, {:num, c}), do: {:num, a * c}
 
-
-  # Performs division between two expressions, handling both numeric and rational types.
-  # Raises an ArgumentError if an attempt is made to divide by zero.
+  # Perform division between two expressions, handling both integers and rational numbers.
+  # Raise an ArgumentError if an attempt is made to divide by zero.
   defp division({:q, a, b}, {:q, c, d}) do
     multiplication({:q, a, b}, {:q, d, c})
   end
@@ -111,25 +107,25 @@ defmodule Expression do
     raise ArgumentError, message: "Attempted to divide by zero."
   end
 
+  # Simplify rational numbers into integers when applicable.
   defp division({:num, n}, {:num, n}), do: {:num, 1}
   defp division({:num, n}, {:num, 1}), do: {:num, n}
   defp division({:num, n1}, {:num, n2}) do
-    # Simplifies the result by dividing both numerator and denominator by their GCD.
+    # Simplify the result by dividing both numerator and denominator by their GCD.
     gcd = Integer.gcd(convert_float_to_int_if_whole(n1), convert_float_to_int_if_whole(n2))
     {:q, convert_float_to_int_if_whole(n1/gcd), convert_float_to_int_if_whole(n2/gcd)}
   end
 
-  # Converts a float to an integer if it's a whole number, otherwise raises an ArgumentError.
+  # Convert a float to an integer if it's a whole number, otherwise raise an ArgumentError.
   defp convert_float_to_int_if_whole(float) do
     truncated = trunc(float)
-    if truncated == float do
-      truncated
-    else
-      raise ArgumentError, message: "Input #{float} is not a whole number and cannot be converted to an integer."
+    if truncated == float do truncated
+    else raise ArgumentError,
+        message: "Input #{float} is not a whole number and cannot be converted to an integer."
     end
   end
 
-  # Pretty printing
+  # Pretty print
   @spec pretty_print(expr()) :: String.t()
   defp pretty_print({:num, n}), do: "#{n}"
   defp pretty_print({:var, v}), do: "#{v}"
