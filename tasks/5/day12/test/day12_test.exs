a6a8faef166d79b04519d4f9e9678970e325ac2b
VincentFerrigan <vincent.ferrigan@gmail.com>
2024-02-16 15:17:39 +0100

    Adding tests for Part II

--- tasks/5/day12/test/day12_test.exs
+++ tasks/5/day12/test/day12_test.exs
@@ -2,9 +2,8 @@ defmodule Day12Test do
   use ExUnit.Case
   doctest Day12
 
-
   describe "Part 1" do
-    test "Test samples", do:
+    test "Test brute force samples", do:
       assert Day12.run_sample("???.### 1,1,3") == 1
       assert Day12.run_sample(".??..??...?##. 1,1,3") == 4
       assert Day12.run_sample("?#?#?#?#?#?#?#? 1,3,1,6") == 1
@@ -13,4 +12,13 @@ defmodule Day12Test do
       assert Day12.run_sample("?###???????? 3,2,1") == 10
   end
 
+  describe "Part 2" do
+    test "Test dp samples", do:
+      assert Day12.run_dynamic_sample("???.### 1,1,3", 1) == 1
+      assert Day12.run_dynamic_sample(".??..??...?##. 1,1,3") == 4
+      assert Day12.run_dynamic_sample("?#?#?#?#?#?#?#? 1,3,1,6") == 1
+      assert Day12.run_dynamic_sample("????.#...#... 4,1,1") == 1
+      assert Day12.run_dynamic_sample("????.######..#####. 1,6,5") == 4
+      assert Day12.run_dynamic_sample("?###???????? 3,2,1") == 10
+  end
 end
