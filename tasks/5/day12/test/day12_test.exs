0aae884c290a925c57069ed7d8105a88f44581b2
VincentFerrigan <vincent.ferrigan@gmail.com>
2024-02-16 18:10:58 +0100

    Updating tests

--- tasks/5/day12/test/day12_test.exs
+++ tasks/5/day12/test/day12_test.exs
@@ -13,12 +13,12 @@ defmodule Day12Test do
   end
 
   describe "Part 2" do
-    test "Test dp samples", do:
-      assert Day12.run_dynamic_sample("???.### 1,1,3", 1) == 1
-      assert Day12.run_dynamic_sample(".??..??...?##. 1,1,3") == 4
-      assert Day12.run_dynamic_sample("?#?#?#?#?#?#?#? 1,3,1,6") == 1
-      assert Day12.run_dynamic_sample("????.#...#... 4,1,1") == 1
-      assert Day12.run_dynamic_sample("????.######..#####. 1,6,5") == 4
-      assert Day12.run_dynamic_sample("?###???????? 3,2,1") == 10
+    test "Test dynamic programming samples, extended multiplier = 5", do:
+      assert Day12.run_dynamic_sample("???.### 1,1,3", 5) == 1
+      assert Day12.run_dynamic_sample(".??..??...?##. 1,1,3", 5) == 16384
+      assert Day12.run_dynamic_sample("?#?#?#?#?#?#?#? 1,3,1,6", 5) == 1
+      assert Day12.run_dynamic_sample("????.#...#... 4,1,1", 5) == 16
+      assert Day12.run_dynamic_sample("????.######..#####. 1,6,5", 5) == 2500
+      assert Day12.run_dynamic_sample("?###???????? 3,2,1", 5) == 506250
   end
 end
