41d4356d188e2d7efff797d7051f9e0430ef89e9
VincentFerrigan <vincent.ferrigan@gmail.com>
2024-02-23 13:31:58 +0100

    Adding test for part 2

--- tasks/6/day05/test/day05_test.exs
+++ tasks/6/day05/test/day05_test.exs
@@ -6,11 +6,15 @@ defmodule Day05Test do
     assert Day05.parse_seeds("seeds: 79 14 55 13") == [79,14,55,13]
   end
 
-  test "Parse sample" do
+  test "Run sample for Part 1" do
     assert Day05.part_1("./input/sample") == 35
   end
 
-  test "Part 1" do
-    assert Day05.part_1("./input/part_1_input") == 282277027
+  test "Run imput for Part 1" do
+    assert Day05.part_1("./input/input") == 282277027
+  end
+
+  test "Run sample for Part 2" do
+    assert Day05.part_2("./input/sample") == 46
   end
 end
