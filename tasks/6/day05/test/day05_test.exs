ce4a4be2cca2b3f64fe3ce1f5ef638947a5e5956
VincentFerrigan <vincent.ferrigan@gmail.com>
2024-02-23 14:29:07 +0100

    Adding test for part 2, input tagged without timeout

--- tasks/6/day05/test/day05_test.exs
+++ tasks/6/day05/test/day05_test.exs
@@ -6,6 +6,11 @@ defmodule Day05Test do
     assert Day05.parse_seeds("seeds: 79 14 55 13") == [79,14,55,13]
   end
 
+  test "merge overlapping ranges" do
+    ranges = [5..15, 3..20, 30..40, 50..70, 35..45 ]
+    assert Day05.merge_overlapping(ranges) |> Enum.sort == [3..20, 30..45, 50..70]
+  end
+
   test "Run sample for Part 1" do
     assert Day05.part_1("./input/sample") == 35
   end
@@ -18,12 +23,8 @@ defmodule Day05Test do
     assert Day05.part_2("./input/sample") == 46
   end
 
+  @tag timeout: 600_000
   test "Run input for Part 2" do
     assert Day05.part_2("./input/input") == 11554135
-  end
-
-  test "merge overlapping ranges" do
-    ranges = [5..15, 3..20, 30..40, 50..70, 35..45 ]
-    assert Day05.merge_overlapping(ranges) |> Enum.sort == [3..20, 30..45, 50..70]
   end
 end
