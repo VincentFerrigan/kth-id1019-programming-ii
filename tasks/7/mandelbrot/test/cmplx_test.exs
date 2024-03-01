b7705b60c537806a4cd7c608f3aecf4fd4ad83d6
VincentFerrigan <vincent.ferrigan@gmail.com>
2024-03-01 13:39:36 +0100

    Refactoring test

--- tasks/7/mandelbrot/test/cmplx_test.exs
+++ tasks/7/mandelbrot/test/cmplx_test.exs
@@ -2,11 +2,9 @@ defmodule CmplxTest do
   use ExUnit.Case
   doctest Cmplx
 
-  test "Create complex number 1+2i, given its real (1) and its imaginary part (2)" do
+  test "Create complex number 1+2i", do:
     assert Cmplx.new(1, 2) == {:cpx, 1, 2}
-  end
 
-  test "Square a complex number" do
+  test "Square a complex number", do:
     assert Cmplx.sqr({:cpx, 1, 2}) == {:cpx, -3, 4}
-  end
 end
