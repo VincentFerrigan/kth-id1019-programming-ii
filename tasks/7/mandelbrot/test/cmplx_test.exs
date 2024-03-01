25cd7f662d41361498e01a4f13368457a110ca0d
VincentFerrigan <vincent.ferrigan@gmail.com>
2024-03-01 15:06:41 +0100

    Refactor

--- tasks/7/mandelbrot/test/cmplx_test.exs
+++ tasks/7/mandelbrot/test/cmplx_test.exs
@@ -4,7 +4,6 @@ defmodule CmplxTest do
 
   test "Create complex number 1+2i", do:
     assert Cmplx.new(1, 2) == {:cpx, 1, 2}
-
   test "Square a complex number", do:
     assert Cmplx.sqr({:cpx, 1, 2}) == {:cpx, -3, 4}
 end
